64a195c9fd7ff3b4cbb04c4093fc664d8dce0413
rodriguezfacundo <facundorodriguez4141@gmail.com>
2022-04-25 19:51:09 -0300

    subida del challenge

---
+++ data/data.js
@@ -0,0 +1,39 @@
+export const data = [
+    {
+      question: "Cual de las siguientes ciudades es la capital de Argentina?",
+      choices: ["Mendoza", "Salta", "Buenos Aires", "Mar del Plata"],
+      answer: "Buenos Aires",
+    },
+    {
+      question: "Cuales de estos colores no es primario",
+      choices: ["Rojo", "Amarillo", "Azul", "Naranja"],
+      answer: "Naranja",
+    },
+    {
+      question: "Cual es el rio mas largo de latinoamerica?",
+      choices: ["Amazonas", "Rio de la Plata", "Rio Parana", "Rio Orinoco"],
+      answer: "Amazonas",
+    },
+    {
+      question: "En que año se tiró el muro de Berlin?",
+      choices: ["2000", "1989", "2001", "1900"],
+      answer: "1989",
+    },
+    {
+      question: "Quien fue el primer presidente del Uruguay?",
+      choices: ["Fructuoso Rivera", "Jose Mujica", "Luis Lacalle Pou", "Gervasio Artigas"],
+      answer: "Fructuoso Rivera",
+    },
+    {
+      question: "¿Quién pintó Las meninas?",
+      choices: ["Francisco de Goya", "Diego Velazquez", "Salvador Dali", "Louis Harry"],
+      answer: "Francisco de Goya",
+    },
+    {
+      question: "En que año se tiró el muro de Berlin?",
+      choices: ["2000", "1989", "2001", "1900"],
+      answer: "1989",
+    },
+    
+    
+  ];
---
+++ models/UI.js
@@ -0,0 +1,42 @@
+export class UI {
+    constructor() {}
+
+    /**
+   *
+   * @param {sting} text
+   */
+    showQuestion(text) {
+        const questionTitle = document.getElementById("question");
+        questionTitle.innerHTML = text;
+    }
+
+    showChoices(choices, callback) {
+        const choicesContainer = document.getElementById("choices");
+        choicesContainer.innerHTML = "";
+
+        for (let i = 0; i < choices.length; i++) {
+            const button = document.createElement("button");
+            button.addEventListener("click", () => callback(choices[i]));
+            button.className = "button";
+            button.innerText= choices[i];
+
+            choicesContainer.append(button);
+        }
+    }
+
+    showScores(score) {
+        const gameOverHTML = `
+        <h1>Result</h1>
+        <h2 id="score">Tu puntaje es: ${score}</h2>
+        `;
+
+        const element = document.getElementById("quiz");
+        element.innerHTML = gameOverHTML
+    }
+
+    showProgress(currentIndex, total) {
+        var element = document.getElementById("progress");
+        element.innerHTML = `Pregunta ${currentIndex} de ${total}`;
+    }
+
+}
